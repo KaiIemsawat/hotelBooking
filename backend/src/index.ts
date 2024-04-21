88caa989c682e61ff0160d1e01cf1a0766b33bc1
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-04-21 17:37:48 -0400

    base backend

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response) => {
+    res.json({ message: "This is express endpoint..!" });
+});
+
+app.listen(3303, () => {
+    console.log("SERVER HAS LAUNCH ON PORT 3303");
+});
