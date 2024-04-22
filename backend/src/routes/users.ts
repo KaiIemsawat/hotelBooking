b3d3ec37158c2eda70329a00aef4beda80a6cb1b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-04-21 22:52:04 -0400

    register route -- jwt, cookie

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
 import User from "../models/user";
+import exp from "constants";
 
 const router = express.Router();
 
+//* /api/users/register
 router.post("/register", async (req: Request, res: Response) => {
     try {
         let user = await User.findOne({
@@ -15,8 +19,25 @@ router.post("/register", async (req: Request, res: Response) => {
 
         user = new User(req.body);
         await user.save();
+
+        const token = jwt.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET_KEY as string,
+            {
+                expiresIn: "1d",
+            }
+        );
+
+        res.cookie("auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 86400000, // 24 * 60 * 60 * 100 millisecond for 1 day
+        });
+        return res.sendStatus(200);
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: "Something went wrong" });
     }
 });
+
+export default router;
