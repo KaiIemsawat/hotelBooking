ad0dd40b8a93381ced7306a7c64c5a2c3d8fc686
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-04-22 22:45:50 -0400

    register page (UI)

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -9,7 +9,7 @@ const Register = () => {
         confirmPassword: string;
     };
 
-    const { register } = useForm<RegisterFormData>();
+    const { register, watch } = useForm<RegisterFormData>();
 
     return (
         <form className="flex flex-col gap-5">
@@ -36,6 +36,54 @@ const Register = () => {
                     />
                 </label>
             </div>
+            <label className="text-gray-700 text-sm font-bold flex-1">
+                Email
+                <input
+                    type="email"
+                    className="border rounded w-full py-1 px-2 font-normal"
+                    {...register("email", {
+                        required: "This field is required",
+                    })}
+                />
+            </label>
+            <label className="text-gray-700 text-sm font-bold flex-1">
+                Password
+                <input
+                    type="password"
+                    className="border rounded w-full py-1 px-2 font-normal"
+                    {...register("password", {
+                        required: "This field is required",
+                        minLength: {
+                            value: 6,
+                            message: "Password must be at least 6 characters",
+                        },
+                    })}
+                />
+            </label>
+            <label className="text-gray-700 text-sm font-bold flex-1">
+                Confirm Password
+                <input
+                    type="password"
+                    className="border rounded w-full py-1 px-2 font-normal"
+                    {...register("confirmPassword", {
+                        validate: (value) => {
+                            if (!value) {
+                                return "This filed is required";
+                            } else if (watch("password") !== value) {
+                                return "Your confirm password is not match";
+                            }
+                        },
+                    })}
+                />
+            </label>
+            <span>
+                <button
+                    type="submit"
+                    className="bg-blue-600 text-stone-50 py-2 px-4 rounded-md font-bold hover:bg-blue-400 text-xl"
+                >
+                    Create Account
+                </button>
+            </span>
         </form>
     );
 };
