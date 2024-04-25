8a2184f936d8ed9132b9f180371e24a02b299bcd
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-04-25 09:30:40 -0400

    signOut button UI

---
+++ frontend/src/components/SignOutButton.tsx
@@ -0,0 +1,8 @@
+const SignOutButton = () => {
+    return (
+        <button className="text-blue-600 hover:text-stone-50 px-3 font-bold hover:bg-stone-300/50 hover:rounded-md">
+            Sign Out
+        </button>
+    );
+};
+export default SignOutButton;
