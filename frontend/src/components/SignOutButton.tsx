6ffda8fd3012a948e71c76e19c23f50f90946d57
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-04-25 09:44:29 -0400

    Sign out function, fix sign out auth route

--- frontend/src/components/SignOutButton.tsx
+++ frontend/src/components/SignOutButton.tsx
@@ -1,6 +1,30 @@
+import { useMutation } from "react-query";
+
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+
 const SignOutButton = () => {
+    const { showToast } = useAppContext();
+
+    const mutation = useMutation(apiClient.signOut, {
+        onSuccess: () => {
+            // Show toast
+            showToast({ message: "Signed Out", type: "SUCCESS" });
+        },
+        onError: (error: Error) => {
+            // Show toast
+            showToast({ message: error.message, type: "ERROR" });
+        },
+    });
+
+    const handleClick = () => {
+        mutation.mutate();
+    };
     return (
-        <button className="text-blue-600 hover:text-stone-50 px-3 font-bold hover:bg-stone-300/50 hover:rounded-md">
+        <button
+            onClick={handleClick}
+            className="text-blue-600 hover:text-stone-50 px-3 font-bold hover:bg-stone-300/50 hover:rounded-md"
+        >
             Sign Out
         </button>
     );
